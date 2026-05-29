4bec2516559a11f15dc5c94c02fd7dcd5e64d54e
Julien LEROY <julien.leroy.work@gmail.com>
2026-05-29 15:41:48 +0200

    Create entities models

---
+++ Api/Atelier.Api/_Entities/PlayerData.cs
@@ -0,0 +1,17 @@
+﻿namespace Atelier.Api._Entities
+{
+    public class PlayerData
+    {
+        public int Id { get; set; }
+        public int Rank { get; set; }
+        public int Points { get; set; }
+        public int Weight { get; set; }
+        public int Height { get; set; }
+        public int Age { get; set; }
+
+        public int PlayerId { get; set; }
+        public Player Player { get; set; }
+
+        public ICollection<PlayerLastResult> LastResults { get; set; } = new List<PlayerLastResult>();
+    }
+}
