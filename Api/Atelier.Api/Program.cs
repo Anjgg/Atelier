70ed5423926dcc37c19517298b62ba5b870eb49b
Julien LEROY <julien.leroy.work@gmail.com>
2026-05-29 16:07:51 +0200

    Add seeder

--- Api/Atelier.Api/Program.cs
+++ Api/Atelier.Api/Program.cs
@@ -1,8 +1,20 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Hosting;
+using Atelier.Api._Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite("Data Source=Tennis.db"));
+
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    context.Database.EnsureCreated();
+    Seeder.Seed(context);
+}
 
 app.MapGet("/", () => "Atelier.Api running");
 
